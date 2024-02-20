6e3e53ecf949b3ced7005f7fccd44230e157f9c1
Darren Dhanpat <darren.dhanpat@yahoo.com>
2024-02-20 09:52:06 -0500

    Completed classes exercise 1

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Create new instance of Car class
+            var redSkodaCar = new Car();
+
+            //set values for the properties of the car instance
+            redSkodaCar.Year = 2024;
+            redSkodaCar.Make = "Skoda";
+            redSkodaCar.Model = "Suberb iV";
+
+            Console.WriteLine($"{redSkodaCar.Year}, {redSkodaCar.Make}, {redSkodaCar.Model}");
+
+            Console.ReadLine();
         }
     }
 }
